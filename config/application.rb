3882b36ce8016e439ee91543bb43d7183d614cd5
Anes Hodza <anes.hodza@renuo.ch>
2023-01-19 19:31:10 +0100

    Add devise and home controller

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,15 @@ module Ananke
     config.i18n.default_locale = :en
 
     config.generators.test_path = 'spec'
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
