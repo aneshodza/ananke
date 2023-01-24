5968a1a95ce6d5196d31818f290d86c1bc969577
anes hodza <75726773+aneshodza@users.noreply.github.com>
2023-01-24 22:23:16 +0100

    Add attributes to user (#6)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  DEVISE_ATTRS = %i[first_name last_name pfp_url].freeze
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: DEVISE_ATTRS)
+    devise_parameter_sanitizer.permit(:account_update, keys: DEVISE_ATTRS)
+  end
+
 end
