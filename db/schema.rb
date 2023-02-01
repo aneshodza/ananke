5ffc170afa2787c38ba33d346aff63c036fb1a06
Anes Hodza <anes.hodza@renuo.ch>
2023-02-01 23:47:10 +0100

    Add PO

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_153830) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_01_224003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_153830) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "project_owner_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -27,6 +28,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_153830) do
     t.integer "project_id"
     t.integer "assigned_user_id"
     t.integer "creator_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 user = User.create!(email: '1@1.com', password: '123456', first_name: 'anes', last_name: 'hodza')
 User.create!(email: '2@2.com', first_name: 'test', last_name: 'user', password: 'ultra_secure')
 
-project = Project.create!(name: 'Project 1')
+project = Project.create!(name: 'Project 1', project_owner: user)
 
 Task.create!(name: 'Task 1', project: project, creator: user)
 Task.create!(name: 'Task 2', project: project, creator: user)
